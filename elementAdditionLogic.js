baeed3a6111989bf486b3e4368ec155c65f1cf05
marejonc <ma_jonczyk@wp.pl>
2021-11-23 19:37:01 +0100

    Usuniecie zbednego kodu sluzacego do prezentacji dodawania pierwiastkow.

--- elementAdditionLogic.js
+++ elementAdditionLogic.js
@@ -7,6 +7,3 @@ function drawElement(elementInfo)
 {
     atomsList.push(new Atom(elementInfo.symbol, elementInfo.hexColor, elementInfo.valences[0]))
 }
-
-document.getElementById("elementAddition").addEventListener("click", () => drawElement(getElementInfo("C")))
-document.getElementById("elementAddition1").addEventListener("click", () => drawElement(getElementInfo("Li")))
