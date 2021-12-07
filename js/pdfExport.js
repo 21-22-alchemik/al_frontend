b3a426cf34a9472fa1e327e2b48129ab6b5d3937
Jakub Figler <jakub.figler@gmail.com>
2021-12-07 18:58:50 +0100

    poprawki wygladu walidacji i exportu

--- js/pdfExport.js
+++ js/pdfExport.js
@@ -5,7 +5,7 @@ function CreatePDFfromHTML() {
     console.log(HTML_Width);
     var HTML_Height = $("#wizualizacjaDanych").height();
     console.log(HTML_Height);
-    var top_left_margin = 15;
+    var top_left_margin = 50;
     var PDF_Width = HTML_Width + (top_left_margin * 2);
     var PDF_Height = (PDF_Width * 1.5) + (top_left_margin);
     var canvas_image_width = HTML_Width;
@@ -16,12 +16,12 @@ function CreatePDFfromHTML() {
     html2canvas($("#wizualizacjaDanych")[0]).then(function (canvas) {
         var imgData = canvas.toDataURL("image/jpeg", 1.0);
         var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+        pdf.text(250, 20, 'Wyeksportowny plik z wynikiem twojej pracy!');
         pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
         for (var i = 1; i <= totalPDFPages; i++) { 
             pdf.addPage(PDF_Width, PDF_Height);
             pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
         }
         pdf.save("wiazaniaPDF.pdf");
-        // $("#wizualizacjaDanych").hide();
     });
 }
