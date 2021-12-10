168ca1acec46fa1b618939f477b49be3235278d3
phabrzyk <phabrzyk@autnomous-systems.pl>
2021-12-10 22:20:20 +0100

    add algo and test

--- .github/tests/my.test.js
+++ .github/tests/my.test.js
@@ -1,15 +1,21 @@
-test('2 + 3 = 5', () => {
-  expect(2 + 3).toBe(5);
-});
+const functions = require('../../algorytm/al_smiles.js')
 
-test('3 * 4 = 12', () => {
-  expect(3 * 4).toBe(12);
-});
 
-test('5 - 6 = -1', () => {
-  expect(5 - 6).toBe(-1);
-});
+atomsList = new Array();
+atomsList.push([ '0', 'O', 2, 85, 325 ]);
+atomsList.push([ '1', 'C', 3, 85, 325 ]);
+atomsList.push([ '2', 'O', 2, 85, 325 ]);
+atomsList.push([ '3', 'O', 2, 85, 325 ]);
+atomsList.push([ '4', 'O', 2, 85, 325 ]);
+connectionList = new Array();
+connectionList.push([ 1, 0 ]);
+connectionList.push([ 2, 1 ]);
+connectionList.push([ 0, 2 ]);
+connectionList.push([ 1, 3 ]);
+connectionList.push([ 4, 1 ]);
+connectionList.push([ 3, 4 ]);
 
-test('8 / 4 = 2', () => {
-  expect(8 / 4).toBe(2);
+
+test('O1OC21(OO2)', () => {
+  expect(al_smiles(atomsList,connectionList)).toBe("O1OC21(OO2)");
 });
