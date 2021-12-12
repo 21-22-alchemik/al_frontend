d4dc51eed9ac6c45eb11cffd59edee80b8a42295
Jakub Figler <jakub.figler@gmail.com>
2021-12-12 21:11:05 +0100

    100% dziala: walidacja, menu, pdfExport, pierwiastki

--- js/pdfExport.js
+++ js/pdfExport.js
@@ -8,8 +8,6 @@ function CreatePDFfromHTML() {
     var top_left_margin = 50;
     var PDF_Width = HTML_Width + (top_left_margin * 2);
     var PDF_Height = (PDF_Width * 1.5) + (top_left_margin);
-    // var canvas_image_width = HTML_Width;
-    // var canvas_image_height = HTML_Height;
 
     var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
 
@@ -17,10 +15,10 @@ function CreatePDFfromHTML() {
         var imgData = canvas.toDataURL("image/jpeg", 1.0);
         var pdf = new jsPDF("p", "pt", [PDF_Width, PDF_Height]);
         pdf.text("Wyeksportowny plik z wynikiem twojej pracy!", (HTML_Width + 100) / 2, 30, "center");
-        pdf.addImage(imgData, "JPG", top_left_margin, top_left_margin, 780, 540);
+        pdf.addImage(imgData, "JPG", top_left_margin, top_left_margin, 1480, 800);
         for (var i = 1; i <= totalPDFPages; i++) { 
             pdf.addPage(PDF_Width, PDF_Height);
-            pdf.addImage(imgData, "JPG", top_left_margin, -(PDF_Height*i)+(top_left_margin*4),780,540);
+            pdf.addImage(imgData, "JPG", top_left_margin, -(PDF_Height*i)+(top_left_margin*4),1480,800);
         }
         pdf.save("wiazaniaPDF.pdf");
     });
