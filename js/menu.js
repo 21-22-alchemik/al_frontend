8838b550e7c45b63745aa1ace0f457b015bdb5b1
Pawel <31793531+PriestOfAdanos@users.noreply.github.com>
2021-12-13 16:23:13 +0100

    working version backup (#9)
    * Demo systemu manipulacji strukturą wzorów
    
    * zapomniana klasa css
    
    * Update super-linter.yml
    
    * Update super-linter.yml
    
    * Delete super-linter.yml
    
    * using lighter image
    
    * add Jest test
    
    * corrcted yaml
    
    * correct workflow
    
    * yaml edits ep 1000
    
    * name change
    
    * random experitment
    
    * lint
    
    * create package.json
    
    * remove dependancy
    
    * remove more reduntant stuff
    
    * menu v1, zakladki, wyglad, brak funckjonalności
    
    * walidacja v1 (przycisk/podpięcie)
    
    * Literówka (valEnce nie valAnce)
    
    * Redukcja z-indeksów dla atomów
    
    * Dodanie pliku przedstawiajacego uklad okresowy
    
    * menu v1.2(wyswietlanie, zmiana wartosci)
    
    * Zamiana rozszerzenia pliku z lista pierwiastkow. Stworzenie logiki dodawania do obszaru roboczego pierwiastka o okreslonym symbolu.
    
    * Zmiany w pliku index.html
    
    * menu prawie gotowe
    
    * poprawki
    
    * Usuniecie zbednego kodu sluzacego do prezentacji dodawania pierwiastkow.
    
    * menu bez check()
    
    * add config for es lint
    
    * add rules to use new config
    
    * linter changes
    
    * Poprawki w JSie (check(), optymalizacja zindexów)
    
    * all codebase validate
    
    * Nowy branch do połączenia menu z mendelejewem
    
    * Zmiana nazwy pliku
    
    * linter
    
    * linter
    
    * gałąź do zmergowania
    
    * dupa
    
    * XD
    
    * dzialajaca wersja
    
    * Kompeltny merge z AL-40 i jej pochodnym branchem
    
    * move html linting to eslint
    
    * correct eslint config
    
    * yyyy
    
    * turn off html validation for while
    
    * poprawki po błędych które linter zrobił
    
    * add trailning empty line
    
    * another line
    
    * poprawka yaml
    
    * make test wait until lint done
    
    * initialize variable and change kebab camel
    
    * add missing rules keyword
    
    * remove > in css
    
    * turn off font check
    
    * asdas
    
    * zapomniane deklarowanie zmiennej
    
    * brakujące pliki z merge
    
    * eslint
    
    * linter
    
    * linter fix
    
    * space to tab
    
    * asd
    
    * linter
    
    * minor correction
    
    * trash
    
    * duuuużo zmian, nie zmieści sie w 50 znakach
    
    * zmiana w eslincie
    
    * rem ,
    
    * Organizacja i integracja kodu
    
    * Podpięcie mendelejewa + optymalizacja menu
    
    * Moj merge z menu
    
    * Działające menu pierwiastki, przykładowe związki
    
    * Pierwsze prace nad narzedziami edytorskimi
    
    * Początki colorPickera, walencja na atomach
    
    * Logika zmieniania koloru dla atomu
    
    * Pobieranie koloru atomu
    
    * Linter config dla windows
    
    * Blokada dodania wiązania przy wyczerpanej walencji
    
    * Próba ogarnięcia lokalnego lintera
    
    Co-authored-by: Mateusz Kowalski <matekow341@student.polsl.pl>
    Co-authored-by: phabrzyk <phabrzyk@autnomous-systems.pl>
    Co-authored-by: Kuba Figler <jakub.figler@gmail.com>
    Co-authored-by: marejonc <ma_jonczyk@wp.pl>
    Co-authored-by: Kuba Figler <48696080+KubaF00@users.noreply.github.com>
    Co-authored-by: marejonc <56203149+marejonc@users.noreply.github.com>

---
+++ js/menu.js
@@ -0,0 +1,126 @@
+/* eslint-disable no-unused-vars */
+
+//sprawdzanie rozmiaru okan przegladarki
+function checkSize() {
+    //wczytanie wartosci okna przegladarki
+    var width = window.innerWidth;
+    var newDivMessage = document.getElementById("newDivMessage");
+    //min 1000px, sprawdzenie
+    if(width < 1000){
+        newDivMessage.classList.remove("activeBadSize");
+    } else {
+        newDivMessage.classList.add("activeBadSize");
+    }
+}
+
+//sprawdzenie co zmiane szerokosci okna przegladarki
+window.addEventListener("resize", checkSize);
+
+// MENU
+
+//wczytanie elementów
+var menuWrapper = document.getElementById("menuWrapper");
+var btnMenu = document.getElementById("menu_menu");
+var btnPierwiastki = document.getElementById("menu_pierwiastki");
+var btnZwiazki = document.getElementById("menu_zwiazki");
+var zakladkaMenu = document.getElementById("zakladkaMenu");
+var zakladkaPierwiastki = document.getElementById("zakladkaPierwiastki");
+var zakladkaZwiazki = document.getElementById("zakladkaZwiazki");
+var blockScreen = document.getElementById("blockScreen");
+
+//podpięcia akcji
+btnMenu.addEventListener("click", menuControl.bind(null,"menu_menu"));
+btnPierwiastki.addEventListener("click", menuControl.bind(null,"menu_pierwiastki"));
+btnZwiazki.addEventListener("click", menuControl.bind(null,"menu_zwiazki"));
+document.getElementById("wyjscie").addEventListener("click", closeMenu);
+blockScreen.addEventListener("click", closeMenu);
+
+function menuControl(element){
+    //jeśli menu jest zamknięte
+    if(menuWrapper.classList.contains("menuClosed")) {
+        changeTab(element);
+        openMenu();
+    }
+    //jeśli jest otwarte
+    else {
+        //jeśli jest otwarte i jest na obecnej zakładce
+        if(document.getElementById(element).classList.contains("activeMenu")) {
+            changeTab("close");
+        }
+        //jeśli zmieniamy karte
+        else{
+            changeTab(element);
+        }
+    }
+}
+
+function changeTab(element){
+
+    if(element!="close"){
+        //czyszczenie 
+        btnMenu.classList.remove("activeMenu");
+        btnPierwiastki.classList.remove("activeMenu");
+        btnZwiazki.classList.remove("activeMenu");
+
+        zakladkaMenu.classList.remove("activeZakladka");
+        zakladkaPierwiastki.classList.remove("activeZakladka");
+        zakladkaZwiazki.classList.remove("activeZakladka");
+    }
+
+    switch(element){
+
+    //menu Menu
+    case "menu_menu":{
+        btnMenu.classList.add("activeMenu");
+        zakladkaMenu.classList.add("activeZakladka");
+        break;
+    }
+
+    //menu Pierwiastki
+    case "menu_pierwiastki":{
+        btnPierwiastki.classList.add("activeMenu");
+        zakladkaPierwiastki.classList.add("activeZakladka");
+        break;
+    }
+
+    //menu Związki
+    case "menu_zwiazki":{
+        btnZwiazki.classList.add("activeMenu");
+        zakladkaZwiazki.classList.add("activeZakladka");
+        break;
+    }
+
+    //jeśli zamykamy menu niezależnie od otwartej karty, albo wydarzy sie dziwny błąd i bedzie miało otworzyć zakładke której nie ma, to zamknie menu
+    default:{
+        closeMenu();
+    }
+
+    }
+}
+
+function openMenu(){
+    menuWrapper.classList.remove("menuClosed");
+    blockScreen.classList.remove("hidden");
+    setTimeout(function(){ blockScreen.classList.remove("hiddenV"); },10);
+}
+
+// .activeMenu -> kolor zakładki
+
+//zamykanie menu - przycisk
+function closeMenu() {
+    menuWrapper.classList.add("menuClosed");
+
+    btnMenu.classList.remove("activeMenu");
+    btnPierwiastki.classList.remove("activeMenu");
+    btnZwiazki.classList.remove("activeMenu");
+    blockScreen.classList.add("hiddenV");
+
+    //zniknij po schowaniu menu
+    setTimeout(function(){
+        zakladkaMenu.classList.remove("activeZakladka");
+        zakladkaPierwiastki.classList.remove("activeZakladka");
+        zakladkaZwiazki.classList.remove("activeZakladka");
+        blockScreen.classList.add("hidden");
+    },500);
+    
+}
