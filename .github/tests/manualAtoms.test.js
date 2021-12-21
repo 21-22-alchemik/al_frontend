efdf9a159a62f8dee4a8c64a82254c71937ddf51
Mateusz Kowalski <matekow341@student.polsl.pl>
2021-12-21 13:10:28 +0100

    test

--- .github/tests/manualAtoms.test.js
+++ .github/tests/manualAtoms.test.js
@@ -128,117 +128,75 @@ var connsHolder = document.getElementById("connsHolder");
 var atomsList = new Array();
 var connsList = new Array();
 
-
 var zIndexVal = 3;
 
 
 //testing adding atoms
-atomsList.push(new Atom("H", "black", 1));
 
 test('AtomAdd', () => {
-    expect(atomsList.length).toEqual(1);
-});
-
-test('AtomAddDOM', () => {
-    expect(atomsHolder.children.length).toEqual(1);
-});
-
-atomsList.push(new Atom("H", "black", 1));
-
-test('SecondAtomAdd', () => {
+    atomsList.push(new Atom("H", "black", 1));
+    expect(atomsList.length).toBe(1);
+    expect(atomsHolder.children.length).toBe(1);
+    atomsList.push(new Atom("H", "black", 1));
     expect(atomsList.length).toBe(2);
-});
-
-test('SecondAtomAddDOM', () => {
     expect(atomsHolder.children.length).toBe(2);
 });
 
 
 //testing connections
 
-connection(atomsList[0],atomsList[1]);
 
 test('ConnectionAdd', () => {
+
     expect(connsList.length).toBe(1);
-});
-
-test('ConnectionAddDOM', () => {
     expect(connsHolder.children.length).toBe(1);
-});
-
-test('ConnectionValue', () => {
     expect(connsList[0].count).toBe(1);
-});
-
-test('AtomConnectionValue', () => {
     expect(atomsList[0].check()).toBe(0);
-});
-
-test('SecondAtomConnectionValue', () => {
     expect(atomsList[1].check()).toBe(0);
 });
 
 //second connection
-connection(atomsList[0],atomsList[1]);
 
 test('SecondConnectionAdd', () => {
+    atomsList.push(new Atom("H", "black", 1));
+    atomsList.push(new Atom("H", "black", 1));
+    connection(atomsList[0],atomsList[1]);
+    connection(atomsList[0],atomsList[1]);
+
     expect(connsList.length).toBe(1);
-});
-
-test('SecondConnectionAddDOM', () => {
     expect(connsHolder.children.length).toBe(1);
-});
-
-test('SecondConnectionValue', () => {
     expect(connsList[0].count).toBe(2);
-});
-
-test('AtomSecondConnectionValue', () => {
     expect(atomsList[0].check()).toBe(1);
-});
-
-test('SecondAtomSecondConnectionValue', () => {
     expect(atomsList[1].check()).toBe(1);
 });
 
 //third connection - reversed
-connection(atomsList[1],atomsList[0]);
+test('SecondConnectionAdd', () => {
+    atomsList.push(new Atom("H", "black", 1));
+    atomsList.push(new Atom("H", "black", 1));
+    connection(atomsList[0],atomsList[1]);
+    connection(atomsList[0],atomsList[1]);
+    connection(atomsList[1],atomsList[0]);
 
-test('ThirdConnectionAdd', () => {
     expect(connsList.length).toBe(1);
-});
-
-test('ThirdConnectionAddDOM', () => {
     expect(connsHolder.children.length).toBe(1);
-});
-
-test('ThirdConnectionValue', () => {
     expect(connsList[0].count).toBe(3);
-});
-
-test('AtomThirdConnectionValue', () => {
     expect(atomsList[0].check()).toBe(2);
-});
-
-test('SecondAtomThirdConnectionValue', () => {
     expect(atomsList[1].check()).toBe(2);
 });
 
 
 //atoms position test
 //default position
-test('AtomDOMLeft', () => {
+test('AtomDOM', () => {
+    atomsList.push(new Atom("H", "black", 1));
     expect(atomsList[0].DOM.offsetLeft).toBe(5);
-});
-test('AtomDOMTop', () => {
     expect(atomsList[0].DOM.offsetTop).toBe(5);
 });
 
 //custom position
-atomsList.push(new Atom("H","black",1,10,20));
-test('CustomAtomDOMLeft', () => {
+test('CustomAtomDOM', () => {
+    atomsList.push(new Atom("H","black",1,10,20));
     expect(atomsList[0].DOM.offsetLeft).toBe(10);
-});
-test('CustomAtomDOMTop', () => {
     expect(atomsList[0].DOM.offsetTop).toBe(20);
 });
