ff9c89a30e3503c9d5ec76061c05ebf8d8f1719c
Jakub Figler <jakub.figler@gmail.com>
2022-01-02 22:55:38 +0100

    testy-wstepnie

---
+++ .github/tests/walidacja.test.js
@@ -0,0 +1,14 @@
+// TEST WALIDACJA ATOMY
+const Atom = require('../../js/polaczenia.js');
+
+test('Wszystko dobrze', () => {
+    //
+});
+  
+test('Za duzo wiazan', () => {
+    //
+});
+
+test('Za malo wiazan', () => {
+    //
+});
