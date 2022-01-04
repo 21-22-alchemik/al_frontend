139703380d61b931810e9e05fdfb029d87f94158
marejonc <ma_jonczyk@wp.pl>
2022-01-04 18:37:06 +0100

    delete redundant stuff

--- .github/tests/dist/editButtons.test.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var editButtons = require('../../js/editButtons.js');
-
-averageExample();
-addConnection.dispatchEvent(new Event("click"));
-var atom_0 = document.getElementById("atom_0");
-atom_0.dispatchEvent(new Event("mousedown"));
-atom_0.dispatchEvent(new Event("mouseup"));
